032b974c4941630f04587dc5944d54446b06d8a7
Bengrezza <bengretza@gmail.com>
2021-10-29 17:13:47 -0600

    Initial Commit

---
+++ src/com/skilldistillery/cards/Card.java
@@ -0,0 +1,53 @@
+package com.skilldistillery.cards;
+
+import java.util.Objects;
+
+public class Card {
+	  private Rank rank;
+	  private Suit suit;
+	  
+	  public Card(Rank rank, Suit suit) {
+	    this.rank = rank;
+	    this.suit = suit;
+	  }
+	  public int getValue() {
+		  return rank.getValue();
+	  }
+
+	  public Rank getRank() {
+		return rank;
+	}
+
+	  public Suit getSuit() {
+			return suit;
+	}
+	  @Override
+	  public String toString() {
+		  return rank + " of " + suit;
+	  }
+	  
+	  @Override
+	  public int hashCode() {
+		return Objects.hash(rank, suit);
+	
+	}
+	 @Override
+	  public boolean equals(Object obj) {
+	    if (this == obj)
+	      return true;
+	    if (obj == null)
+	      return false;
+	    if (getClass() != obj.getClass())
+	      return false;
+	    Card other = (Card) obj;
+	    if (rank != other.rank)
+	      return false;
+	    if (suit != other.suit)
+	      return false;
+	    return true;
+	  }
+
+
+
+}
+
